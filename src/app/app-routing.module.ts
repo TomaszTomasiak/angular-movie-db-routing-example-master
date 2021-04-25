3732b5bfafeb749606f6341147f17f5ad086273e
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-04-25 10:12:46 +0200

    redirecting to page not found component in case of wrong adress put and pathMatch is full

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './pages/movies/movies.component';
+import { MyPageNotFoundComponent } from './pages/page-not-found/my-page-not-found';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/movies', pathMatch: 'full'},
-  {path: 'movies', component: MoviesComponent}
-]
+  {path: 'movies', component: MoviesComponent},
+  {path: '**', component: MyPageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/page-not-found/my-page-not-found.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-page-not-found',
+  template: '<h1>My PAGE NOT FOUND</h1>',
+})
+export class MyPageNotFoundComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
