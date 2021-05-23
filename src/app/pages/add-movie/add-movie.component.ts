234e60b6a7f3d4e27772e4fc16553e1031dedd30
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-05-23 10:56:13 +0200

    Select year & category implemented

--- src/app/pages/add-movie/add-movie.component.ts
+++ src/app/pages/add-movie/add-movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Movie } from 'src/app/models/movie';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-add-movie',
@@ -10,9 +11,14 @@ import { Movie } from 'src/app/models/movie';
 export class AddMovieComponent implements OnInit {
 
   model: Partial<Movie> = {};
-  constructor() { }
+  categories: string[] = [];
+  years: string[] = [];
+  
+  constructor(public http: HttpService) { }
 
   ngOnInit(): void {
+    this.http.getCategories().subscribe(categories => this.categories = categories);
+    this.http.getYears().subscribe(years => this.years = years);
   }
 
   send() {
